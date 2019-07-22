25466b168bf1edef95bb488d715ede40a50c5325
Donald Whitely <dswhitely1@gmail.com>
2019-07-22 09:24:32 -0400

    Change Routes

--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@ const cool = require('cool-ascii-faces');
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const todosRouter = require('../routers/todosRouter.js');
 const authRouter = require('../routers/authRouter.js');
 const usersRouter = require('../routers/usersRouter.js');
@@ -10,24 +11,23 @@ const authorization = require('../routers/authorization.js');
 const server = express();
 
 server.use(helmet());
-server.use(express.json());
+server.use(bodyParser.urlencoded({ extended: false }));
+server.use(bodyParser.json());
 server.use(cors());
 
-server.use('/api/todos', authorization, todosRouter); //todos list array 
+server.use('/api/todos', authorization, todosRouter); //todos list array
 server.use('/api/auth', authRouter); //register and login (receive token)
 server.use('/api/users', authorization, usersRouter); //user profile (view, edit, delete)
 
-
 //server test
 server.get('/', async (req, res) => {
-    res.send(`Welcome to the Wunderlist 2.0 API. Local to production testing!`)
+	res.send(`Welcome to the Wunderlist 2.0 API. Local to production testing!`);
 });
-
 
 // simple GET check that displays a random ascii face
-server.get('/cool', (req, res) => {  // Deployment check
-    res.send(cool());
+server.get('/cool', (req, res) => {
+	// Deployment check
+	res.send(cool());
 });
-
 
 module.exports = server;
--- data/helpers/users-model.js
+++ data/helpers/users-model.js
@@ -1,61 +1,52 @@
 const db = require('../dbConfig.js');
 
 module.exports = {
-    find,
-    findById,
-    findByUser,
-    create,
-    remove,
-    update
+	find,
+	findById,
+	findByUser,
+	create,
+	remove,
+	update,
 };
 
 async function find() {
-    const users = await db("users");
-    return users;
+	const users = await db('users');
+	return users;
 }
 
 async function findById(id) {
-    const user = await db("users")
-        .where({ "users.id": id })
-        .first();
-      return user;
+	const user = await db('users').where({ 'users.id': id }).first();
+	const { password, ...rest } = user;
+	return rest;
 }
 
 async function findByUser(username) {
-    const user = await db("users")
-        .where({ username })
-        .first()
-    return user;
+	const user = await db('users').where({ username }).first();
+	return user;
 }
 
 async function create(item) {
-    const [id] = await db("users")
-        .insert(item)
-        .returning("id");
-    if (id) {
-        const user = await findById(id);
-        return user;
-    }
+	const [ id ] = await db('users').insert(item).returning('id');
+	if (id) {
+		const user = await findById(id);
+		return user;
+	}
 }
 
 async function remove(id) {
-    const user = await findById(id);
-    if (user) {
-        const deleted = await db("users")
-            .where({ id })
-            .del();
-        if (deleted) {
-            return user;
-        }
-    }
+	const user = await findById(id);
+	if (user) {
+		const deleted = await db('users').where({ id }).del();
+		if (deleted) {
+			return user;
+		}
+	}
 }
 
 async function update(item, id) {
-    const editedUser = await db("users")
-        .where({ id })
-        .update(item);
-    if (editedUser) {
-      const user = await findById(id);
-      return user;
-    }    
+	const editedUser = await db('users').where({ id }).update(item);
+	if (editedUser) {
+		const user = await findById(id);
+		return user;
+	}
 }
